a87be553b3f7021e10493e9f325f3c8b9f98df85
Pajjaphon Whanchid <97209810+ZFlucKZ@users.noreply.github.com>
2024-01-03 11:08:48 +0700

    Add : Auth Test

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,3 +1,34 @@
+import type { User } from '@prisma/client'
+
 Cypress.Commands.add('dataCy', (value) => {
   return cy.get(`[data-cy=${value}]`)
+})
+
+// Cypress.Commands.add('containArticles', (articles) => {
+//   for (const article of articles) {
+//     cy.dataCy(`article-item-${article.id}`).within(() => {
+//       cy.dataCy('article-item-title').should('have.text', article.title)
+//       cy.contains(article.excerpt)
+//       cy.dataCy('article-item-more-details').should('contain','More Details')
+//     })
+//   }
+// })
+
+Cypress.Commands.add('loginAs', (role) => {
+  return cy.task('xlsx-to-json', 'users/all.xlsx').then((users: User[]) => {
+    const user: User = Cypress._.sample(users.filter(u => u.role === role))
+    
+    cy.task('db:users:upsert', user)
+
+    cy.visit('/auth/login')
+    cy.dataCy('email').type(user.email)
+    cy.dataCy('password').type(user.password)
+    cy.dataCy('submit').click()
+    cy.contains('You have already been logged in')
+  })
+})
+
+Cypress.Commands.add('logout', () => {
+  cy.get('header [aria-haspopup="menu"]').click()
+  cy.contains('Logout').click()
 })
